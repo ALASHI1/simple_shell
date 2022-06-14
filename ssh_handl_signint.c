d58d7547d8437f29351d657f200fb8940babce12
JadeTFC <101394926+JadeTFC@users.noreply.github.com>
2022-06-14 14:28:31 +0100

    task

--- ssh_handl_signint.c
+++ ssh_handl_signint.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * handl_signint - Handle Ctrl + c call in prompt.
+ * ssh_handl_signint - Handle Ctrl + c call in prompt.
  *
  * @sig: Signal
  */
-void handl_signint(int sig)
+void ssh_handl_signint(int sig)
 {
 	(void) sig;
 	write(STDOUT_FILENO, "\n#cisfun$ ", 11);
