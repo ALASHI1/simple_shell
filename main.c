3e0f69fa0a57a12697025cb2ca9e6a26d5edf212
JadeTFC <101394926+JadeTFC@users.noreply.github.com>
2022-06-14 15:03:21 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * main - Runs the shell
  *
@@ -7,8 +6,13 @@
  */
 int main(void)
 {
+	shell_t shell;
+
+	initialize(&shell);
 	signal(SIGINT, ssh_handl_signint);
 	ssh_repl();
+	ssh_repl(&shell);
+	uninitialize(&shell);
 
 	return (0);
 }
