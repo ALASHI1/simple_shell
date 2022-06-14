0a7fe648a9aa74d322b4457ee5736c26be14d35b
JadeTFC <101394926+JadeTFC@users.noreply.github.com>
2022-06-14 14:31:30 +0100

    task

--- ssh_prompt.c
+++ ssh_prompt.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- *  prompt - prints the prompt
+ *  ssh_prompt - prints the prompt
  */
-void prompt(void)
+void ssh_prompt(void)
 {
 	write(STDOUT_FILENO, "#cisfun$ ", 10);
 }
