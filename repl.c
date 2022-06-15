391751167c487df6e09655adba5094cd570ddd4c
ALASHI1 <alashimuyiwa@gmail.com>
2022-06-15 16:06:10 +0100

    updated

--- repl.c
+++ repl.c
@@ -1,31 +1,35 @@
 #include "shell.h"
 
 /**
- * ssh_repl - Read-Eval-Print-Loop for the shell
- * 
+ * repl - Read-Eval-Print-Loop for the shell
+ *
  * @shell: shell data
  */
-void ssh_repl(shell_t *shell)
+void repl(shell_t *shell)
 {
 	char *cmd;
-	int chr;
+	int chr, flag, isLoop, tcheck;
 
+	tcheck = isatty(STDIN_FILENO);
 	do {
-		ssh_prompt();
+		isLoop = 1;
+		if (tcheck == 1)
+			prompt();
 
 		cmd = read_line(&chr);
 		if (chr == -1)
 		{
 			free(cmd);
-			write(STDOUT_FILENO, "\n", 2);
-			exit(EXIT_SUCCESS);
+			if (tcheck == 1)
+				write(STDOUT_FILENO, "\n", 2);
+			return;
 		}
 
 		if (cmd == NULL)
 			continue;
 
-        parse_command(shell, cmd);
-
-		free(cmd);
-	} while (1);
+		flag = parse_command(shell, cmd);
+		if (flag == EXIT_IND)
+			isLoop = 0;
+	} while (isLoop);
 }
--- shell.h
+++ shell.h
@@ -1,35 +1,85 @@
-#include "shell.h"
+#ifndef SHELL_H
+#define SHELL_H
+
+#define BUFSIZE 1024
+#define TOKEN_BUFSIZE 128
+#define TOKEN_DELIM " \t\r\n\a"
+#define EXIT_IND 2
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
 
 /**
- * repl - Read-Eval-Print-Loop for the shell
+ * struct shell - shell data for current session
  *
- * @shell: shell data
+ * @argv: argument vector
+ * @command: input command
+ * @environ: env variables
+ * @name: the program name
+ * @count: counts number of processes
+ * @exitcode: the value the code exits
+ * @pid: pid of current process
  */
-void repl(shell_t *shell)
+typedef struct shell
 {
-	char *cmd;
-	int chr, flag, isLoop, tcheck;
+	char **argv;
+	char *command;
+	char **environ;
+	char *name;
+	int count;
+	int exitcode;
+	char *pid;
+} shell_t;
 
-	tcheck = isatty(STDIN_FILENO);
-	do {
-		isLoop = 1;
-		if (tcheck == 1)
-			prompt();
+/**
+ * struct builtin - builtins
+ *
+ * @name: name of builtin
+ * @handl: builtin handler
+ */
+typedef struct builtin
+{
+	char *name;
+	int (*handl)(shell_t *shell);
+} builtin_t;
 
-		cmd = read_line(&chr);
-		if (chr == -1)
-		{
-			free(cmd);
-			if (tcheck == 1)
-				write(STDOUT_FILENO, "\n", 2);
-			return;
-		}
+extern char **environ;
 
-		if (cmd == NULL)
-			continue;
+/* setup */
+void initialize(shell_t *shell, char **av);
+void uninitialize(shell_t *shell);
+void handl_signint(int sig);
+void repl(shell_t *shell);
 
-		flag = parse_command(shell, cmd);
-		if (flag == EXIT_IND)
-			isLoop = 0;
-	} while (isLoop);
-}
+/* env */
+char *_getenv(char **_environ, const char *name);
+int _setenv(shell_t *shell, char *name, char *value, int overwrite);
+
+/* executor */
+int parse_command(shell_t *shell, char *command);
+int execute(shell_t *shell);
+
+/* builtins */
+int (*get_builtin_handl(const char *cmd))(shell_t *shell);
+int hsh_exit(shell_t *shell);
+int hsh_env(shell_t *shell);
+int hsh_cd(shell_t *shell);
+int hsh_setenv(shell_t *shell);
+int hsh_unsetenv(shell_t *shell);
+
+/* utils */
+void prompt(void);
+char *read_line(int *chr);
+char **tokenize(char *s);
+char *analyze_cmd(shell_t *shell, char *cmd);
+char *exp_variables(shell_t *shell, char *s);
+
+/* error */
+void write_error(shell_t *shell, int status);
+
+#endif /* SHELL_H */
